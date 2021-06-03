400a1f6b660db7577d34c72ded888c242afa0d11
jabuna07 <jabuna007@gmail.com>
2021-06-03 18:44:08 +0545

    completed patch api method

--- src/components/LeftContent.jsx
+++ src/components/LeftContent.jsx
@@ -8,6 +8,11 @@ export default class LeftContent extends Component {
         super();
         this.state = {
             user: '',
+            bio: '',
+            company: '',
+            location: '',
+            name: '',
+            twitterUserName: '',
             isInEditMode: false
         }
     }
@@ -19,11 +24,51 @@ export default class LeftContent extends Component {
     fetchUser = async () => {
         const response = await fetch(' https://api.github.com/users/jabuna07')
         const data = await response.json()
+        console.log(data.twitter_username,'----')
         this.setState({
             user: data,
+            bio: data.bio,
+            company: data.company,
+            location: data.location,
+            name: data.name,
+            twitterUserName: data.twitter_username,
+
+
         });
 
     };
+
+    updateUserDetail = async () => {
+        const response = await fetch('https://api.github.com/user', {
+            method: 'PATCH',
+            headers: new Headers({
+                'Authorization': 'Bearer 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',
+                'Content-Type': 'application/json',
+            }),
+
+            body: JSON.stringify({
+                bio: this.state.bio,
+                company: this.state.company,
+                location: this.state.location,
+                name: this.state.name,
+                twitter_username: this.state.twitterUserName,
+            })
+        });
+
+        const data = await response.json()
+        this.setState({
+            user: data,
+            bio: data.bio,
+            company: data.company,
+            location: data.location,
+            name: data.name,
+            twitterUserName: data.twitter_username,
+
+        })
+
+    };
+
+
 
 
     getUserLogin = () => {
@@ -58,7 +103,41 @@ export default class LeftContent extends Component {
         console.log("saved")
     }
 
+
+    handleChange = (event) => {
+        // console.log(event.target.value,'----')
+        this.setState({
+            bio: event.target.value
+        })
+    }
+
+    handleCompanyChange = (event) => {
+        this.setState({
+            company: event.target.value
+        })
+    }
+
+    handleLocationChange = (event) => {
+        this.setState({
+            location: event.target.value
+        })
+    }
+
+    handleemailChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleTwitterNameChange = (event) => {
+        this.setState({
+            twitterUserName: event.target.value
+        })
+    }
+
     renderEditView = () => {
+
+        console.log(this.state.user.bio, this.state.location, '------')
         return <> <div className="content">
             <div className="content__image">
                 <img src="avatar.png" alt="image" />
@@ -71,16 +150,16 @@ export default class LeftContent extends Component {
 
             <div className="content__userDetail">
 
-                <textarea className="Bio" cols="35" rows="6" placeholder="Add a Bio"></textarea>
+                <textarea className="Bio" cols="35" rows="6" placeholder="Add a Bio" onChange={this.handleChange} value={this.state.bio}></textarea>
                 <form action="detail">
-                    <input type="text" placeholder="company" />
-                    <input type="text" placeholder="location" />
-                    <input type="text" placeholder="website" />
-                    <input type="text" placeholder="twitter username" />
+                    <input type="text" placeholder="company" onChange={this.handleCompanyChange} value={this.state.company} />
+                    <input type="text" placeholder="location" onChange={this.handleLocationChange} value={this.state.location} />
+                    <input type="text" placeholder="name" onChange={this.handleemailChange} value={this.state.name} />
+                    <input type="text" placeholder="twitter username" onChange={this.handleTwitterNameChange} value={this.state.twitterUserName} />
                 </form>
             </div>
             <div className="content__button">
-                <input type="button" value="Save" className="content__button--save" onClick={this.updateMode} />
+                <input type="button" value="Save" className="content__button--save" onClick={this.updateUserDetail} />
                 <input type="button" value="Cancel" className="content__button--cancel" onClick={this.editMode} />
             </div>
         </>
@@ -103,6 +182,7 @@ export default class LeftContent extends Component {
 
 
     render() {
+
         return (
             this.state.isInEditMode ? this.renderEditView() : this.renderDefaultView()
         )
